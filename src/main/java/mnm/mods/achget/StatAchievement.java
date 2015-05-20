407f3acd711c83220cef8c3764b9df13d58652c9
Matthew Messinger <mattmess1221@gmail.com>
2015-05-19 20:37:27 -0400

    Add support for itemstack meta.
    Move some inner classes to new files.

--- src/main/java/mnm/mods/achget/StatAchievement.java
+++ src/main/java/mnm/mods/achget/StatAchievement.java
@@ -1,7 +1,6 @@
 package mnm.mods.achget;
 
 import net.minecraft.event.HoverEvent;
-import net.minecraft.item.ItemStack;
 import net.minecraft.stats.Achievement;
 import net.minecraft.stats.AchievementList;
 import net.minecraft.stats.StatBase;
@@ -10,7 +9,6 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
-import cpw.mods.fml.common.registry.GameRegistry;
 
 public class StatAchievement extends Achievement {
 
@@ -22,7 +20,7 @@ public class StatAchievement extends Achievement {
     private EnumChatFormatting color;
 
     public StatAchievement(JsonAchievement json) throws ParentNotLoadedException, StatNotFoundException {
-        super(json.id, "", json.xPos, json.yPos, getItem(json.item), getParent(json.parent));
+        super(json.id, "", json.xPos, json.yPos, json.item, getParent(json.parent));
         this.stat = StatList.getOneShotStat(json.stat);
         if (stat == null) {
             throw new StatNotFoundException("Stat '" + json.stat + "' does not exist.");
@@ -52,16 +50,6 @@ public class StatAchievement extends Achievement {
         if (!AchievementGet.instance.achievements.containsKey(parent))
             throw new ParentNotLoadedException();
         return AchievementGet.instance.achievements.get(parent);
-    }
-
-    private static ItemStack getItem(String item) {
-        String modid = "minecraft";
-        String name = item;
-        if (item.contains(":")) {
-            modid = item.substring(0, item.indexOf(':'));
-            name = item.substring(item.indexOf(':') + 1);
-        }
-        return new ItemStack(GameRegistry.findItem(modid, name));
     }
 
     public StatBase getStat() {
@@ -100,45 +88,6 @@ public class StatAchievement extends Achievement {
     @Override
     public String getDescription() {
         return this.description;
-    }
-
-    public static class JsonAchievement {
-
-        private String id;
-        private IChatComponent name;
-        private String desc;
-        private String parent;
-        private String stat;
-        private String item = "minecraft:apple";
-        private String color = "gray";
-        private boolean special;
-        private int count;
-        private int xPos;
-        private int yPos;
-
-        public String getID() {
-            return id;
-        }
-
-        public String getParent() {
-            return parent;
-        }
-
-    }
-
-    public static class ParentNotLoadedException extends Exception {
-
-        private static final long serialVersionUID = 6733700654373877073L;
-
-    }
-
-    public static class StatNotFoundException extends Exception {
-
-        private static final long serialVersionUID = -578829147493724218L;
-
-        public StatNotFoundException(String msg) {
-            super(msg);
-        }
     }
 
 }
---
+++ src/main/java/mnm/mods/achget/StatNotFoundException.java
@@ -0,0 +1,10 @@
+package mnm.mods.achget;
+
+public class StatNotFoundException extends Exception {
+
+    private static final long serialVersionUID = -578829147493724218L;
+
+    public StatNotFoundException(String msg) {
+        super(msg);
+    }
+}
